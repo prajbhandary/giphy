a8cb530f9f9696894226f2f2bbbd3c4aca5c3e20
Pranab Rajbhandary <rajbhandaryp@gmail.com>
2018-10-25 23:40:57 -0500

    fixed again

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -23,7 +23,7 @@ function displayGiphy() {
   //console.log("h1")
   var apiKey = "4P6oQNQRGxmm4QoNauZjOM2RJJxejQWT"
   var giphy = $(this).attr("data-name");
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +giphy + "&api_key="+ apiKey + "&limit=10"
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +giphy + "&api_key="+ apiKey + "&limit=10"
   
   $.ajax({
     url : queryURL,
